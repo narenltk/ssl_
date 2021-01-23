ec33b152cfbff012be2d1e9403254535b39ca58e
Narendiran Krishnan <narenltk@gmail.com>
2021-01-23 09:47:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    res.send('Did you google narenltk ?')
+});
+
+const server = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
+
+server.listen(3030, () => console.log('narenltk running on port 3030'));
